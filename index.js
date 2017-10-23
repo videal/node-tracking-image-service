273cae976142c6f63dfd000033ffa180d952f578
varchun <varchun@vardos.net>
2017-10-23 12:07:02 +0300

    ###First commit.

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 /**
  * http://localhost:3000/?source=https://www.google.com.ua/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png
  * */
-const download = require('image-downloader');
-const fs = require('fs');
-const {send} = require('micro');
-const {parse} = require('url');
 
-module.exports = async (request, response) => {
-  const statusCode = 200;
+global.CONFIG = require('./config');
 
+const micro = require('micro');
+const Downloader = require('./downloader');
+
+const server = micro(async (request, response) => {
   /* Parses url and get needed param */
-  let params = parse(request.url, true);
-  if (!params.query.source) {
+  let params = require('url').parse(request.url, true);
+  if (!params.query[CONFIG.parameterName]) {
     return;
   }
-
-  /* Checks is path_to_save exist */
-  let tmpDist = './tmp';
-  if (!fs.existsSync(tmpDist)) {
-    fs.mkdirSync(tmpDist);
+  
+  let url = params.query[CONFIG.parameterName];
+  let image = await Downloader.image({url: url, cacheTime: 30});
+  if (!image) {
+    micro.send(response, 404);
   }
+  micro.send(response, 200, image.body);
+});
 
-  /* Gets an image and saves it */
-  let {newFileName, image} = await download.image({url: params.query.source, dest: tmpDist});
-
-  console.log('newFileName = ', newFileName);
-  console.log('image = ', image);
-
-  send(response, statusCode);
-};
+server.listen(CONFIG.port);
